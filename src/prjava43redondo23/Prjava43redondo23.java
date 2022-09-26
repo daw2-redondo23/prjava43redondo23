7c1b8f256c89fec95a89d5e0671af83f218fb493
daw2-redondo23 <redondobarrosoruben@fpllefia.com>
2022-09-26 19:51:09 +0200

    Versió 0.2-A del projecte prjava43redondo23. Branca00 creada i modificada.

--- src/prjava43redondo23/Prjava43redondo23.java
+++ src/prjava43redondo23/Prjava43redondo23.java
@@ -32,8 +32,9 @@ public class Prjava43redondo23 extends Frame
         // "this" Frame fires WindowEvent its registered WindowEvent listener 
         // "this" Frame adds "this" object as a WindowEvent listener 
  
-      setTitle("prjava43redondo23");  // "this" Frame sets title 
-      setSize(250, 100);        // "this" Frame sets initial size 
+        
+      setTitle("nova finestra prjava43redondo23");// "this" Frame sets title 
+      setSize(450, 100);        // "this" Frame sets initial size 
       setVisible(true);         // "this" Frame shows 
    } 
  
